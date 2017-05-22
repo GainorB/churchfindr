9485e27de0a33123fdd82dccc49a607ff10f6bfb
Gainor Bostwick <gainorbostwick@gmail.com>
2017-05-21 20:07:10 -0400

    google maps support

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,19 +1,29 @@
+'use strict'
 console.log('map.js is connected');
 
 const locations = [];
+let newObj = { lat: 0, lng: 0 };
 
 axios.get("https://churchfindr.herokuapp.com/api/churches")
     .then((data) => {
         data.data.data.forEach((element) => {
             locations.push({lat: element.lat, lng: element.lng }) 
         });
+
+        // CREATING CENTER COORDINATES FOR GOOGLE MAPS
+        let lat = locations.reduce(function(accumulator, value) { return accumulator + value.lat; }, 0);
+        let lng = locations.reduce(function(accumulator, value) { return accumulator + value.lng; }, 0);
+
+        // CREATE NEW OBJECT WITH LAT AND LNG AND GET THE AVERAGE
+        newObj = { lat: ((lat)/(locations.length)), lng: ((lng)/(locations.length)) };
+
         initMap();
     }).catch((err) => console.log(err));
 
 function initMap(){
     var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 15,
-          center: {lat: 40.63818905, lng: -73.60413015}
+          center: {lat: newObj.lat, lng: newObj.lng}
         });
 
     // Create an array of alphabetical characters used to label the markers.
